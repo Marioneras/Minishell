5ca19d81ab1f8f6587194a4908a2b2bb9c04ba25
mberthou <mberthou@student.42.fr>
2025-05-29 12:05:15 +0200

    fix: tokenisation function now handles quotes correctly

--- parsing/syntax_check.c
+++ parsing/syntax_check.c
@@ -18,26 +18,36 @@ bool	check_pipe(t_token *current)
 		return (false);
 	if (current->previous->type == PIPE || current->next->type == PIPE)
 		return (false);
+	if (current->previous->type == TRUNC || current->previous->type == APPEND
+		|| current->previous->type == INPUT
+		|| current->previous->type == HEREDOC)
+		return (false);
+
 	return (true);
 }
 
-char	*check_redirection(t_token *current)
+static bool	check_redirection(t_token *current)
 {
 	if (current->previous)
 	{
-			if (current->previous->type == TRUNC
+		if (current->previous->type == TRUNC
 			|| current->previous->type == APPEND
 			|| current->previous->type == INPUT
 			|| current->previous->type == HEREDOC)
-			return (current->name);
+		{
+			ft_putstr_fd("mafiyashell: syntax error near unexpected token `", 2);
+			ft_putstr_fd(current->name, 2);
+			ft_putstr_fd("'\n", 2);
+			return (false);
+		}
 	}
-	return ("OK");
+	return (true);
 }
 
 int	redirection_error(char *str)
 {
 	ft_putstr_fd("mafiyashell: syntax error near unexpected token `", 2);
-	if(ft_strlen(str) >= 2)
+	if(ft_strlen(str) >= 4)
 	{
 		ft_putchar_fd(*str, 2);
 		ft_putchar_fd(*str, 2);
@@ -63,8 +73,8 @@ int	check_syntax(t_token *head)
 			return (PIPE_ERROR);
 		if ((current->type == TRUNC || current->type == APPEND
 			|| current->type == INPUT || current->type == HEREDOC)
-			&& ft_strncmp(check_redirection(current), "OK", 3) != 0)
-			return (redirection_error(check_redirection(current)));
+			&& !check_redirection(current))
+			return (INVALID_OPERATOR);
 		if ((current->name[0] == '<' && (current->type != INPUT
 			&& current->type != HEREDOC)) || (current->name[0] == '>'
 			&& (current->type != TRUNC && current->type != APPEND)))
